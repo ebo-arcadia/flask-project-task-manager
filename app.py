3897969486ed953c488e4625ecae957f06d4c442
Ebo Lee <yiboli.coding@gmail.com>
2021-06-07 11:14:21 -0700

    set up database, configuration, file path, create model class with attributes for todo

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, url_for
+from flask.scaffold import F
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+# initialize database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
+# create a model
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
 @app.route('/')
 def index():
     return render_template('index.html')
