d51f4eb61afa4a4b206d6fe5fd66c68f217ae7a6
Ebo Lee <yiboli.coding@gmail.com>
2021-06-07 16:52:02 -0700

    set up update route, find task with id, href send url to app.py with post action, find task, update content of task, update db, redirect

--- app.py
+++ app.py
@@ -48,5 +48,20 @@ def delete(id):
     except:
         return 'there was an issue deleting this task'
 
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        task.content = request.form['content']
+        
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'there was an issue updating the task'
+    else:
+        return render_template('update.html', task=task)
+
 if __name__ == "__main__":
     app.run(debug=True)
